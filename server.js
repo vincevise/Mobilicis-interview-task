383b05c11b6f1fe5b051764bfb60177752e334b6
Vincent Vuram <vincentvuram204@gmail.com>
2023-04-25 21:08:01 +0530

    renamed userRouter.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { connectDB } = require('./config/db')
-const { userRouter } = require('./routes/UserRoutes')
+const { userRouter } = require('./routes/userRouter')
 const cors = require('cors')
 
 const app = express()
@@ -10,7 +10,7 @@ connectDB()
 
 app.use(cors())
 app.use(express.json())
-
+ 
 app.use(express.urlencoded({ extended: true })); 
 
  
@@ -22,6 +22,9 @@ app.use((req, res, next) => {
  
 
 app.use('/api/users',userRouter)
+app.use('/api',(req,res)=>{
+  res.send('badhiya')
+})
  
 
 app.listen(4000, ()=>{
